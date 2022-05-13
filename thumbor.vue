249c7b9dd590f38c49680eecde6d18c3248477ed
mayoreee <mayowaegbinola@gmail.com>
2022-05-14 00:08:13 +0100

    setup: support vue 2 and 3

--- thumbor.vue
+++ thumbor.vue
@@ -1,4 +1,10 @@
 <script>
 // eslint-disable-next-line no-unused-vars
-import { ref, reactive, defineComponent } from "vue-demi";
+import { ref, reactive, defineComponent, isVue3 } from "vue-demi";
+
+if (isVue3) {
+  // Vue 3 only
+} else {
+  // Vue 2 only
+}
 </script>
